b826115832ef8bc8d7b98d095177bef659c6c832
Ballinout123 <john.eastham33@gmail.com>
2015-10-27 09:01:11 -0500

    Our changes

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -465,10 +465,10 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 14:
         if getting_team_name:
-            return 'loyal vengeful occasionally greedy'
+            return 'peter and clayton'
         else:
             if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
+                return 'b'
             elif history[-1]=='c' and opponent_history[-1]=='b':
                 return 'b' # betray is they were severely punished last time
             else:
